ae221e74c5d5245f7607eb878163e9e5565ec6cb
Leonardo Pizzoquero <leonardo.pizzoquero@outlook.com>
2019-11-05 16:24:37 -0300

    Updates

---
+++ precache-manifest.0e3c5a1d62e09d6d39bfc05f5a68b0d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c06c1caf967595785ba2c7d24b46bf5",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "36102391da1b3fa9d689",
+    "url": "/monsters-search/static/css/main.c4c7e541.chunk.css"
+  },
+  {
+    "revision": "849a0e77c7a65ebc01db",
+    "url": "/monsters-search/static/js/2.64cdb3b7.chunk.js"
+  },
+  {
+    "revision": "36102391da1b3fa9d689",
+    "url": "/monsters-search/static/js/main.0c31707b.chunk.js"
+  },
+  {
+    "revision": "83ee0709e40c25debe4b",
+    "url": "/monsters-search/static/js/runtime-main.65ce8a32.js"
+  }
+]);
